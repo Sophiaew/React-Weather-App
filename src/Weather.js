43ff9a3b7da7d74e2898c23e3e2d743e6497f778
Sophiaew <Sophiaew@live.com>
2022-02-04 12:52:45 -0500

    Creation

---
+++ src/Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+import { Circles } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`
+    );
+  }
+  let apiKey = "17bcfd67e085423bef87d025a6a15b1b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div>
+      <h2>Hello!</h2>
+      <Circles color="#00BFFF" height={80} width={80} />
+    </div>
+  );
+}
